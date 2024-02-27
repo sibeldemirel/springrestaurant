471968963774db5e94ea7592ff0dae4d0f2028e0
amine.elalame <elm.dev.code@gmail.com>
2024-02-27 14:01:25 +0100

    feat(restaurant/menu): Add methods to Repository

--- springrestaurant/src/main/java/fr/cda/restaurant/menu/MenuRepository.java
+++ springrestaurant/src/main/java/fr/cda/restaurant/menu/MenuRepository.java
@@ -4,10 +4,18 @@ package fr.cda.restaurant.menu;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 
 public interface MenuRepository extends JpaRepository<Menu, Integer> {
+
+    Optional<Menu> findByNom(String nom);
+
+    // SELECT * FROM menu WHERE restaurant_id IS ...
+    Optional<List<Menu>> findAllByRestaurantId(Integer id);
+
+    // SELECT * FROM menu WHERE realisateur_nom IS ...
+    Optional<List<Menu>> findAllByRestaurantName(String nomRestaurant);
 
 }
