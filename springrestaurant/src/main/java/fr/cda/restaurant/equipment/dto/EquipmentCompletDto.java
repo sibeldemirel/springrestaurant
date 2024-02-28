eff023880651efb0a5ad1b9e169fb0a26464504f
sibel <sibeldmrl19@gmail.com>
2024-02-28 13:16:36 +0100

    feat(equipment): add dto annotations

--- springrestaurant/src/main/java/fr/cda/restaurant/equipment/dto/EquipmentCompletDto.java
+++ springrestaurant/src/main/java/fr/cda/restaurant/equipment/dto/EquipmentCompletDto.java
@@ -1,5 +1,8 @@
 package fr.cda.restaurant.equipment.dto;
 
+import lombok.Data;
+
+@Data
 public class EquipmentCompletDto {
     private Integer id;
     private String name;
--- springrestaurant/src/main/java/fr/cda/restaurant/equipment/dto/EquipmentReduitDto.java
+++ springrestaurant/src/main/java/fr/cda/restaurant/equipment/dto/EquipmentReduitDto.java
@@ -1,6 +1,8 @@
 package fr.cda.restaurant.equipment.dto;
 
+import lombok.Data;
 
+@Data
 public class EquipmentReduitDto {
     private String name;
     private boolean equiped;
