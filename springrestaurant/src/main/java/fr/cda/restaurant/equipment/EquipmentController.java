e71b916af6015915b7d72e0da89adf5d36944ac8
amine.elalame <elm.dev.code@gmail.com>
2024-02-28 15:12:57 +0100

    refactor(cda/restaurant): adjustement

--- springrestaurant/src/main/java/fr/cda/restaurant/equipment/EquipmentController.java
+++ springrestaurant/src/main/java/fr/cda/restaurant/equipment/EquipmentController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/equipment")
+@RequestMapping("/equipments")
 public class EquipmentController {
     private final EquipmentService equipmentService;
 
